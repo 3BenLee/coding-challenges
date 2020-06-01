1f78979bacd503cf3745cfe1a11891d918509440
BenLee3 <bendesigns76@gmail.com>
2020-06-01 21:16:18 +0900

    Solve fizzbuzz challenge

---
+++ fizz-buzz/index.js
@@ -0,0 +1,18 @@
+var sayFizzBuzz = function () {
+  for (let i = 1; i <= 100; i++) {
+    let output = '';
+    if (i % 3 == 0) {
+      output += 'Fizz';
+    }
+    if (i % 5 == 0) {
+      output += 'Buzz';
+    }
+
+    if (output == '') {
+      output = i;
+    }
+    console.log(output);
+  }
+};
+
+console.log(sayFizzBuzz());
